0e84bafe059da9bfdea3d21483a1bfeaa3329b22
Cassandra Newell <cassandra.newell@protonmail.com>
2018-08-05 10:10:55 -0400

    setting up devise and tests

---
+++ app/javascript/react/app.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <Router history={browserHistory}>
+        <Route path='/'>
+
+        </Route>
+      </Router>
+    )
+  }
+}
+
+export default App;
