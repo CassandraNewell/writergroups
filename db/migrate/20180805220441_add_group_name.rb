b4c963f687661e126f6dd0e758217e794077cfc2
Cassandra Newell <cassandra.newell@protonmail.com>
2018-08-06 11:25:13 -0400

    simplify with focus on mvp

--- db/migrate/20180805220441_add_group_name.rb
+++
@@ -1,7 +0,0 @@
-class AddGroupName < ActiveRecord::Migration[5.2]
-  def change
-    change_table :groups do |t|
-      t.string :name, null: false
-    end
-  end
-end
--- db/migrate/20180806115941_remove_ownership.rb
+++
@@ -1,5 +0,0 @@
-class RemoveOwnership < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :groups, :owner, :string
-  end
-end
