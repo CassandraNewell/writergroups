f9529d8964cac65cb977864c36b0edde10ea1519
Cassandra Newell <cassandra.newell@protonmail.com>
2018-08-17 11:06:57 -0400

    refine splash page

--- app/javascript/components/SplashTile.js
+++ app/javascript/components/SplashTile.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react'
 
 const SplashTile = (props) => {
   return(
-    <div className="hero-full-screen grid-x grid-x-padding">
+    <div className="hero-full-screen grid-x grid-padding-x">
       <div className="middle-content-section cell small-6 small-offset-3 grid-y grid-padding-y">
         <div className="cell small-4">
           <h1> Welcome! </h1>
         </div>
+
         <div className="cell small-4">
-          <p> WriterGroups helps writers form writing groups. Find a group -- or create your own -- and then chat with your group, schedule meetups, share book recs, and more! </p>
+          <p>
+            WriterGroups helps writers form writing groups. Find a group -- or create your own -- and then chat with your group, schedule meetups, share book recs, and more!
+          </p>
         </div>
+
         <div className="cell small-4 grid-x">
           <div className="cell small-4 small-offset-1">
             <a className="button expanded" href="/users/sign_in"> Sign In </a>
